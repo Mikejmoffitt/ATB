af03007841e91ed20490089d3fd0d36ee7d9f5bd
Michael Moffitt <mikejmoffitt@gmail.com>
2013-11-06 19:50:07 -0500

    More animation stuff

--- src/faito/faito/Sprite.h
+++ src/faito/faito/Sprite.h
@@ -36,8 +36,43 @@ class Sprite
 {
 public:
 
-	std::string charName
+	std::string charName;
 	Sprite(std::string name);
+
+	// Blits current appropriate frame
+	void blit(int x, int y);
+
+	// Like above, but renders hitboxes
+	void boxBlit(int x, int y, int box);
+	ALLEGRO_BITMAP* getFrame();
+
+	// A single animation tick is activated here
+	void animate();
+
+	// Change the current animation sequence
+	void playAnimation(int num);
+	
+	// Like above, but does not reset frame counter
+	void switchAnimation(int num);
+
+	// Switch animation and force reset counter even if it matches
+	void forceAnimation(int num);
+
+	// Resets whichever animation is playing to given frame (default 0)
+	void resetAnimation(int frame = 0);
+
+	// Gets the length in frames of an animation
+	unsigned int animationLength(int num);
+
+	// Get number of frames of an animation (not duration!)
+	unsigned int numFrames(int num);
+
+private:
+
+	// Animation state variables
+	int animNum; // Which animation is being played
+	int frameNum; // Incremented when frameCnt > length[anim][frame]
+	int frameCnt; // Incremented every tick
 
 	// Each animation property is accessde by array index for the given anim
 	// For example to check whether animation 6 loops I would look at 
@@ -60,7 +95,6 @@ public:
 	std::vector<int> boxY[NUM_BOXES][ANIM_NUM]; // Y offset from top-left
 	std::vector<int> boxW[NUM_BOXES][ANIM_NUM]; // Width
 	std::vector<int> boxH[NUM_BOXES][ANIM_NUM]; // Height
-
 };
 
 
