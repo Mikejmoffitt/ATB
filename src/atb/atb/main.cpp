bd22950fcd4fad0066d2847fce6540efb3c48b51
Samir Mohamed <SamirMohamedCS@gmail.com>
2013-11-09 13:44:55 -0500

    Pseudo charselect, gameloop in main

--- src/atb/atb/main.cpp
+++ src/atb/atb/main.cpp
@@ -37,34 +37,67 @@ int main(int argc, char **argv)
 	al_init_primitives_addon();
 	al_init_image_addon();
 	
+	//Setup Background bitmap
 	std::stringstream sb;
 	sb << STAGE_DIR << "characterselect" << ANIM_FORMAT;
 	ALLEGRO_BITMAP* background = al_load_bitmap(sb.str().c_str());
 
-	Player* example = new Player("chunex",false);
+	//To be made during character select
+	Player* Player1 = NULL;
+	Player* Player2 = NULL;
+	int rounds1= 0,rounds2 = 0;
+	int numRounds = 2;//How many rounds does a player need to win the match
 
 	Barge* barge = new Barge("c:/atb.ini");
-	example->barge = barge;
 	int timer = 0;
+	//Character Select song
 	ALLEGRO_SAMPLE* sample = al_load_sample("data/testmus.ogg");
 	al_play_sample(sample,1,0,1,ALLEGRO_PLAYMODE_LOOP,0);
-	while (timer < 1800)
+
+	//Character Select Loop
+	while ((Player1 == NULL || Player2 == NULL) && timer<200)
 	{
 		barge->poll();
 		al_set_target_bitmap(mainBuffer);
-		//al_clear_to_color(al_map_rgb(255 - timer,(timer * 2) % 255,timer%255));
 		barge->draw(background, 0,0,false,false);
-		// Just shows the animation script does play properly...
 
 		al_set_target_backbuffer(display);
 		al_draw_scaled_bitmap(mainBuffer,
 			0,0,baseX,baseY,0,0,windowX,windowY,0);
 		al_flip_display();
 		timer++;
+		//Needs to be handled by actual character select eventually
+		Player1 = new Player("chunex",false); //Pretend Players selected a character
+		Player2 = new Player("chunex",true);
+		Player1->barge = barge;
+		Player2->barge = barge;
 	}
+	sb.str("");
+	sb << STAGE_DIR << "stage" << ANIM_FORMAT;
+	background = al_load_bitmap(sb.str().c_str());
+
+	//Play Stage Song
+	al_stop_samples();
+	sample = al_load_sample("data/testmus.ogg");
+	al_play_sample(sample,1,0,1,ALLEGRO_PLAYMODE_LOOP,0);
+	//Game Start loop
+	while(rounds1 < numRounds && rounds2 < numRounds)
+	{
+		barge->poll();
+		al_set_target_bitmap(mainBuffer);
+		barge->draw(background, 0,0,false,false);
+
+		al_set_target_backbuffer(display);
+		al_draw_scaled_bitmap(mainBuffer,
+			0,0,baseX,baseY,0,0,windowX,windowY,0);
+		al_flip_display();
+
+	}
+
 	al_stop_samples();
 	al_destroy_sample(sample);
-	delete(example);
+	delete(Player1);
+	delete(Player2);
 	delete(barge);
 	al_destroy_bitmap(mainBuffer);
 	al_destroy_display(display);
