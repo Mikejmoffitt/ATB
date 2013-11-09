20ce57e0116f7ac8f71067d89e2ee813370081f9
Michael Moffitt <mikejmoffitt@gmail.com>
2013-11-08 22:02:57 -0500

    Some stupid art added

--- src/atb/atb/Barge.h
+++ src/atb/atb/Barge.h
@@ -1,3 +1,16 @@
 #pragma once
 
+/* 
+
+The barge class handles inputs and has a wrapper draw function. 
+
+*/
+
 #include <vector>
+#include "allegro5\allegro_image.h"
+#include "allegro5\allegro_primitives.h"
+
+class Barge
+{
+
+};
